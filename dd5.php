e400657290d9f765a5fb50e77f37d8a16ce107a2
Hugues Joneaux <github@jhugues.fr>
2026-09-23 23:08:30 +0200

    v1.26.09.23 - Sonar dd5.php.

--- dd5.php
+++ dd5.php
@@ -6,17 +6,6 @@ define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_PACKAGE', 'DD5');
 session_start([]);
 
-if (false) {
-    // Activer l'affichage des erreurs
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    // Pour voir la stack complète lors d'une exception
-    set_exception_handler(function ($e) {
-        echo "Exception: " . $e->getMessage() . "\n";
-        echo $e->getTraceAsString();
-    });
-}
 
 /**
  * Plugin Name: HJ - DD5
